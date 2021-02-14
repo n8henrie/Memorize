f8ffd6f33b7fdbf41f256d31007993afeadca2b2
Nathan Henrie <nate@n8henrie.com>
2021-02-14 09:33:03 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nathan Henrie on 2021-02-14.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
